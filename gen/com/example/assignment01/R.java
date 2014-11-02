d3a4fb860c8846ac0df50ad704d67750b3dfd03f
Robert Gioia <rag29@njit.edu>
2014-11-02 15:30:30 -0500

    Menu added, working

--- gen/com/example/assignment01/R.java
+++ gen/com/example/assignment01/R.java
@@ -14,28 +14,40 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int SCROLLER_ID=0x7f060009;
-        public static final int edit_MPG=0x7f060005;
-        public static final int edit_files=0x7f060008;
-        public static final int edit_make=0x7f060000;
-        public static final int edit_model=0x7f060001;
-        public static final int edit_outfile1=0x7f060014;
-        public static final int edit_pictureURL=0x7f060006;
-        public static final int edit_position2=0x7f060012;
-        public static final int edit_price=0x7f060004;
-        public static final int edit_text1=0x7f060011;
-        public static final int edit_vin=0x7f060003;
-        public static final int edit_year=0x7f060002;
-        public static final int image_load=0x7f060013;
-        public static final int image_load2=0x7f060007;
-        public static final int text_main=0x7f06000a;
-        public static final int text_main1=0x7f06000b;
-        public static final int text_main2=0x7f06000c;
-        public static final int text_main3=0x7f06000d;
-        public static final int text_main4=0x7f06000e;
-        public static final int text_main5=0x7f06000f;
-        public static final int text_main6=0x7f060010;
-        public static final int text_status1=0x7f060015;
+        public static final int SCROLLER_ID=0x7f070009;
+        public static final int action_settings=0x7f070016;
+        public static final int edit_MPG=0x7f070005;
+        public static final int edit_files=0x7f070008;
+        public static final int edit_make=0x7f070000;
+        public static final int edit_model=0x7f070001;
+        public static final int edit_outfile1=0x7f070014;
+        public static final int edit_pictureURL=0x7f070006;
+        public static final int edit_position2=0x7f070012;
+        public static final int edit_price=0x7f070004;
+        public static final int edit_text1=0x7f070011;
+        public static final int edit_vin=0x7f070003;
+        public static final int edit_year=0x7f070002;
+        public static final int image_load=0x7f070013;
+        public static final int image_load2=0x7f070007;
+        public static final int opt1=0x7f070017;
+        public static final int opt10=0x7f070020;
+        public static final int opt11=0x7f070021;
+        public static final int opt2=0x7f070018;
+        public static final int opt3=0x7f070019;
+        public static final int opt4=0x7f07001a;
+        public static final int opt5=0x7f07001b;
+        public static final int opt6=0x7f07001c;
+        public static final int opt7=0x7f07001d;
+        public static final int opt8=0x7f07001e;
+        public static final int opt9=0x7f07001f;
+        public static final int text_main=0x7f07000a;
+        public static final int text_main1=0x7f07000b;
+        public static final int text_main2=0x7f07000c;
+        public static final int text_main3=0x7f07000d;
+        public static final int text_main4=0x7f07000e;
+        public static final int text_main5=0x7f07000f;
+        public static final int text_main6=0x7f070010;
+        public static final int text_status1=0x7f070015;
     }
     public static final class layout {
         public static final int activity_add=0x7f030000;
@@ -44,6 +56,13 @@ public final class R {
         public static final int activity_load=0x7f030003;
         public static final int activity_main=0x7f030004;
         public static final int activity_write_to_file=0x7f030005;
+    }
+    public static final class menu {
+        public static final int add_item=0x7f060000;
+        public static final int file_load=0x7f060001;
+        public static final int file_write=0x7f060002;
+        public static final int main=0x7f060003;
+        public static final int remove_item=0x7f060004;
     }
     public static final class string {
         public static final int action_settings=0x7f04002f;
--- src/com/example/assignment01/AddActivity.java
+++ src/com/example/assignment01/AddActivity.java
@@ -21,7 +21,7 @@ public class AddActivity extends Activity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.add_item, menu);
+		getMenuInflater().inflate(R.menu.add_item, menu);
 		return true;
 	}
 	
